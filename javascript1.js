3f0a540a0777554ff015c94a375cdb0c4294703c
KarthikKashi98 <52784615+KarthikKashi98@users.noreply.github.com>
2019-09-06 01:40:09 +0530

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,111 @@
+var a=0
+var b=0
+function pointA(){
+    a+=1;
+    document.getElementById("a").value=a;
+
+
+
+}
+function pointB(){
+    b+=1;
+    document.getElementById("b").value=b;
+
+
+
+}
+function myFunction() {
+
+
+    document.getElementById("b1").innerHTML = "X";
+}
+function myFunction1() {
+
+
+    document.getElementById("b1").innerHTML = "O"
+
+}
+function x1() {
+
+
+    document.getElementById("b2").innerHTML = "X"
+}
+function o1() {
+
+
+    document.getElementById("b2").innerHTML = "O"
+}
+function x2() {
+
+
+    document.getElementById("b3").innerHTML = "X"
+
+}
+function o2() {
+
+
+    document.getElementById("b3").innerHTML = "O"
+}
+function x3() {
+
+
+    document.getElementById("b4").innerHTML = "X"
+}
+function o3() {
+
+
+    document.getElementById("b4").innerHTML = "O"
+}
+function x4() {
+
+
+    document.getElementById("b5").innerHTML = "X"
+
+}
+function o4() {
+
+
+    document.getElementById("b5").innerHTML = "O"
+
+}
+function x5() {
+
+
+    document.getElementById("b6").innerHTML = "X"
+}
+function o5() {
+
+
+    document.getElementById("b6").innerHTML = "O"
+
+}
+function x6() {
+
+
+    document.getElementById("b7").innerHTML = "X"
+}
+function o6() {
+
+
+    document.getElementById("b7").innerHTML = "O"
+}
+function x7() {
+
+
+    document.getElementById("b8").innerHTML = "X"
+}
+function o7() {
+
+
+    document.getElementById("b8").innerHTML = "O"
+}
+function x8() {
+
+
+    document.getElementById("b9").innerHTML = "X"
+}
+function o8() {
+
+
+    document.getElementById("b9").innerHTML = "O";
+}
